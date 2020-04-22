ff39ce759cfa30a01206e7a061433d2bb3157fd9
Денис Кирилюк <denkiril@gmail.com>
2020-04-23 01:20:22 +0300

    began to layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tvbit-admin-modules-test';
+  title = 'tvbit';
+  accauntTitle = 'Тестовый аккаунт';
+  userRoleTitle = 'Главный пользователь TVbit';
 }
